8981d9d7d2378402eca6fad2e3a662f22b7c2896
deathmarine <josh@mcjoshmod.com>
2011-11-28 22:38:21 -0500

    Working on Enchantments and levels Working on Economy importing Working on Help
    Signed-off-by: deathmarine <josh@mcjoshmod.com>

--- src/com/modcrafting/identify/commands/IdentifyCommand.java
+++ src/com/modcrafting/identify/commands/IdentifyCommand.java
@@ -3,11 +3,13 @@ package com.modcrafting.identify.commands;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -32,11 +34,15 @@ public class IdentifyCommand implements CommandExecutor {
 		int randprice = config.getInt("prices.randomprice");
 		if (sender instanceof Player){
 			player = (Player)sender;
-			if (Permissions.Security.permission(player, "identify.item")){
+			/*
+			 * if (Permissions.Security.permission(player, "identify.item")){
+			 
 				auth = true;
 			}else{
 			 if (player.isOp()) auth = true; //defaulting to Op if no permissions or node
 			}
+			*Reconstruct Permissions
+			*/
 			admin = player.getName();
 		}else{
 			auth = true;
@@ -48,6 +54,15 @@ public class IdentifyCommand implements CommandExecutor {
 		}
 		if (args.length < 1){
 			ItemStack item = player.getItemInHand();
+			
+			Enchantment eItem = getById();
+			
+			//alright bukkit has no checks for what items can be enchanted
+			
+			boolean checkItem = eItem.canEnchantItem(item);
+			if (checkItem){
+				sender.sendMessage("Your Not Able To Identify That.");				
+			}
 			if (server){
 				sender.sendMessage("Your Not Able To Identify.");
 			}
@@ -59,24 +74,34 @@ public class IdentifyCommand implements CommandExecutor {
 			if(plugin.random){
 				if(args[0].equalsIgnoreCase("buy")){
 					int price = config.getInt("prices.randomprice");
-					
+					//TODO Construct with Vault API for withdraw
+					plugin.addEnchantment(ench, level);
 					return true;
 				}
 			}
 			if(args[0].equalsIgnoreCase("list")){
 				//list available enchantment for item in hand
+				//however no matter what items they might all be the same
 				return true;
 			}
 			if(args[0].equalsIgnoreCase("buy")){
 				int price = config.getInt("prices.levelprice");
+				//TODO Construct with Vault API for withdraw
+				plugin.addEnchantment(ench, level);
 				return true;
 			}
 			if(args[0].equalsIgnoreCase("help")){
+				//TODO Build help system
 				return true;
 			}
 			/*Build Configuration Commands Completed
+			 * Set
+			 * levelprice-done
+			 * randomprice-done
+			 * type-done
 			 * 
 			 */
+			//TODO Move to Set Class on Cleanup
 			if(args[0].equalsIgnoreCase("set")){
 				if(args[1].equalsIgnoreCase("levelprice")){
 					String p = args[2];
@@ -110,8 +135,16 @@ public class IdentifyCommand implements CommandExecutor {
 							sender.sendMessage("Select True or False!");
 							return true;
 						}
-						if(args[3].equalsIgnoreCase("true")) config.set("enchantTools", (boolean) true);
-						if(args[3].equalsIgnoreCase("false")) config.set("enchantTools", (boolean) false);
+						boolean tool = new Boolean(args[3]);
+						if(tool){
+							config.set("enchantTools", (boolean) true);
+							sender.sendMessage("Enchant Tools Set True!");
+							Identify.log.log(Level.INFO, "[Identify] Enchant Tools Set to True!");
+						}else{
+							config.set("enchantTools", (boolean) false);
+							sender.sendMessage("Enchant Tools Set False!");
+							Identify.log.log(Level.INFO, "[Identify] Enchant Tools Set to False!");
+						}
 						plugin.saveConfig();
 						return true;
 					}
@@ -120,8 +153,16 @@ public class IdentifyCommand implements CommandExecutor {
 							sender.sendMessage("Select True or False!");
 							return true;
 						}
-						if(args[3].equalsIgnoreCase("true")) config.set("enchantArmor", (boolean) true);
-						if(args[3].equalsIgnoreCase("false")) config.set("enchantArmor", (boolean) false);
+						boolean armor = new Boolean(args[3]);
+						if(armor){
+							config.set("enchantArmor", (boolean) true);
+							sender.sendMessage("Enchant Armor Set True!");
+							Identify.log.log(Level.INFO, "[Identify] Enchant Armor Set to True!");
+						}else{
+							config.set("enchantArmor", (boolean) false);
+							sender.sendMessage("Enchant Armor Set False!");
+							Identify.log.log(Level.INFO, "[Identify] Enchant Armor Set to False!");
+						}
 						plugin.saveConfig();
 						return true;
 						
@@ -132,13 +173,15 @@ public class IdentifyCommand implements CommandExecutor {
 				sender.sendMessage("/identify set <levelprice/randomprice/type>");
 				return true;
 			}
-			//if () return true;
-			
+			/*
+			 * End of Set
+			 */
 			return true;
 		}
 		if (auth){
 			if (plugin.random){
-				sender.sendMessage("Identify your item for " + randprice);
+				String itemvar = toString();
+				sender.sendMessage("Identify " + itemvar + " for " + randprice);
 				return true;
 			}
 		}
